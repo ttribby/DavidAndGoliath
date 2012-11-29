b182b4e78e9b38da6bc676dfaba9b1ce2de6add4
wpenfold <wpenfold@mines.edu>
2012-11-29 09:27:35 -0700

    made more changes to david

--- src/Main/David.java
+++ src/Main/David.java
@@ -4,7 +4,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
-
+//mmeemememememe
 public class David {
 	public David(){
 		crotchLocationLocation =  new Location(125,175+jumper);
