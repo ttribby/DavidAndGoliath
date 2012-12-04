bb5ee096aa4920dfabc50ae546b88a8155262ff7
ttribby <ttribby@mines.edu>
2012-12-03 17:26:27 -0700

    added QuizWindow and WinWindow

---
+++ src/Main/QuizWindow.java
@@ -0,0 +1,45 @@
+package Main;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class QuizWindow extends JFrame {
+	public QuizWindow(){
+
+		JLabel question = new JLabel("<html>     2 rocks fly in the air at 0 degrees.  <br>    The first one is launched at 50% power and the second is launched at 100% power. <br> Which will hit the ground first? </html>      ");
+		JButton hundred = new JButton("100%");
+		JButton fifty = new JButton("50%");
+		question.setBorder(new TitledBorder(new EtchedBorder(), "Quizzer"));
+		hundred.addActionListener(new HundredListener());
+		fifty.addActionListener(new FiftyListener());
+		add(question, BorderLayout.NORTH);
+		add(hundred,BorderLayout.SOUTH);
+		add(fifty,BorderLayout.CENTER);
+		setSize(800,200);
+		this.pack();
+	}
+	
+	
+	private class HundredListener implements ActionListener{
+		public void actionPerformed(ActionEvent e)
+		{	
+			JOptionPane.showMessageDialog(null, "Your wrong they will both hit the ground at the same time", "Wrong", JOptionPane.QUESTION_MESSAGE);
+			setVisible(false);
+		}
+	}
+	private class FiftyListener implements ActionListener{
+		public void actionPerformed(ActionEvent e)
+		{	
+			JOptionPane.showMessageDialog(null, "Your wrong they will both hit the ground at the same time", "Wrong", JOptionPane.QUESTION_MESSAGE);
+			setVisible(false);		
+		}
+	}
+}
---
+++ src/Main/WinWindow.java
@@ -0,0 +1,32 @@
+package Main;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.smartcardio.Card;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class WinWindow extends JFrame {
+public WinWindow(){
+	setSize(300,300);
+	JLabel wonInfo = new JLabel("You Win!!");
+	wonInfo.setFont(new Font("Serif", Font.PLAIN, 35));
+	JButton ok = new JButton("ok, I am awsome at playing this game");
+	ok.addActionListener(new closePanel());
+	add(ok, BorderLayout.SOUTH);
+	add(wonInfo,BorderLayout.CENTER);
+}
+
+
+private class closePanel implements ActionListener{
+	public void actionPerformed(ActionEvent e)
+	{
+		setVisible(false);
+	}
+}
+
+}
